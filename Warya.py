1872038e876be3d1e9f54ed7cea96b61459db11f
AliRazaWarya <103767516+AliRazaWarya@users.noreply.github.com>
2022-04-15 23:06:41 +0500

    Update Warya.py

--- Warya.py
+++ Warya.py
@@ -7,14 +7,14 @@ except:
 
 import requests
 if not os.path.isfile('Jutt.so'):
-    os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/Warya.cpython-310.so > Warya.so')
+    os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/Jutt.cpython-310.so > Jutt.so')
     os.system('clear')
 if not os.path.isfile('brand.so'):
     os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/brand.cpython-310.so > brand.so')
     os.system('clear')
 bit=platform.architecture()[0]
-go = requests.get('https://raw.githubusercontent.com/AliRazaWarya/Warya/main/update.txt').text
-if 'Warya' in go:
+go = requests.get('https://raw.githubusercontent.com/AliRazaWarya/AliRazaWarya/main/update.txt').text
+if 'Waryabrand' in go:
     if bit == '64bit':
         from Jutt import reg
         reg()
@@ -22,8 +22,8 @@ if 'Warya' in go:
         from brand import reg
         reg()
 else:
-    os.system('rm -rf Warya.so brand.so')
-    os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/Warya.cpython-310.so > Warya.so')
+    os.system('rm -rf Jutt.so brand.so')
+    os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/Jutt.cpython-310.so > Jutt.so')
     os.system('curl -L https://raw.githubusercontent.com/AliRazaWarya/Waryabrand/main/brand.cpython-310.so > brand.so')
     if bit == '64bit':
         from Jutt import reg
